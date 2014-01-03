d8d8cc6e4c2424f52f9aa1b37472ddb1f6ad37f7
Henrique Gusso <henrique@gus.so>
2014-01-03 17:59:38 -0200

    payment radio inputs, icons and inactive behavior

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 $(function() {
 
 	$('label.radio').click(function(){
-		$('input[name=entrega]').parents('label').not(this).addClass('fadeout').removeClass('checked');
+		if ($(this).closest('.form-section').hasClass('inactive')) {
+		    return;
+		}
+		if ($(this).closest('.form-section').hasClass('activate')) {
+		    $('.inactive').removeClass('inactive');
+		}
+		if ($(this).find('input[name=payment][value=boleto]').length) {
+		    $('p').show();
+		} else if ($(this).find('input[name=payment]').length) {
+			$('p').hide();
+		}
+		$(this).closest('.form-section').find('.radio').not(this).addClass('fadeout').removeClass('checked');
 		$(this).removeClass('fadeout').addClass('checked');
 	});
 });
