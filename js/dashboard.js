bbef485329fd3433c6baf23d3bf3d5e98968fe9b
Amilton L Paglia <amilton@matilhaestud.io>
2014-01-14 18:40:29 -0200

    added iCheck for checkbox and radio

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,19 +4,26 @@
 $(function() {
 
   // navigation behavior
-  function mainNav() {
-    if( $('.main-nav').length ) {
-      
-      $('.main-nav')
-        .mouseover(function() {
-          $(this).addClass('open');
-        })
-        .mouseout(function() {
-          $(this).removeClass('open');
-        });
+    function mainNav() {
+      if( $('.main-nav').length ) {
+        
+        $('.main-nav')
+          .mouseover(function() {
+            $(this).addClass('open');
+          })
+          .mouseout(function() {
+            $(this).removeClass('open');
+          });
+      }
     }
-  }
 
+  // iCheck
+    if($('input').length) {
+      $('input').iCheck({
+        checkboxClass: 'icheckbox',
+        radioClass: 'iradio'
+      });
+    }
 
   // init 
 
